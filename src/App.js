370c30767a0e347fdbabd9012e0a2120aae01769
Azay Genius <azay.genius8@gmail.com>
2022-02-19 01:07:28 +0000

    Main App component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/header.jsx';
+import Phone from './components/Phone.jsx';
+// import Phone from './components/Phone.jsx';
+import People from './components/People';
+// import Footer from './components/Footer';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Header/>
+            <Phone/>
+            <People/>
+        </div>
+    );
 }
 
 export default App;
