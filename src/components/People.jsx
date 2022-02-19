7b36b82ad827f30b22e0953037d72443e5682626
Azay Genius <azay.genius8@gmail.com>
2022-02-19 01:08:09 +0000

    Employee section

---
+++ src/components/People.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components';
+
+function People() {
+  return (
+    <Section>
+    <div>
+    People
+    </div>
+
+    </Section>
+  )
+}
+
+export default People;
+const Section = styled.section`
+
+
+
+`;
