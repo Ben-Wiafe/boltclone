914dda068f2903489a4888f56fdfa474880fcaa7
Azay Genius <azay.genius8@gmail.com>
2022-02-24 23:57:16 +0000

    final commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import Header from './components/header.jsx';
-import Phone from './components/Phone.jsx';
-import People from './components/People';
-import Footer from './components/Footer';
-
+import React from "react";
+import Header from "./components/header.jsx";
+import Phone from "./components/Phone.jsx";
+import People from "./components/People";
+import Footer from "./components/Footer";
 
 function App() {
-    return ( <
-        div >
-        <
-        Header / >
-        <
-        Phone / >
-        <
-        People / >
-        <
-        Footer / >
-        <
-        /div>
-    );
+  return (
+    <div>
+      <Header />
+      <Phone />
+      <People />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
